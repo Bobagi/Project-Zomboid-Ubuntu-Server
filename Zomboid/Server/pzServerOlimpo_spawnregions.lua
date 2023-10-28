9095f5f0a98c7085c9aac29a29af18e93ee3153e
Gustavo Perin <gustavoperin067@gmail.com>
2023-10-28 06:58:02 -0300

    Addind the Zomboid/Server/servername.ini sample

---
+++ Zomboid/Server/pzServerOlimpo_spawnregions.lua
@@ -0,0 +1,8 @@
+function SpawnRegions()
+	return {
+		{ name = "01 - Muldraugh, KY", file = "media/maps/Muldraugh, KY/spawnpoints.lua" },
+		{ name = "02 - Riverside, KY", file = "media/maps/Riverside, KY/spawnpoints.lua" },
+		{ name = "03 - Rosewood, KY", file = "media/maps/Rosewood, KY/spawnpoints.lua" },
+		{ name = "04 - West Point, KY", file = "media/maps/West Point, KY/spawnpoints.lua" },
+	}
+end
